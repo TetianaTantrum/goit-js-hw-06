cdf03d6c5df08f3faeb8fa05d66939401bd5f508
TetianaTantrum <tetiana.tantrum@gmail.com>
2022-12-06 19:49:18 +0200

    9 tasks

--- js/task-04.js
+++ js/task-04.js
@@ -1,8 +1,3 @@
-// Создай переменную counterValue в которой будет храниться текущее значение счетчика и
-// инициализируй её значением 0.
-// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
-// Обновляй интерфейс новым значением переменной counterValue.
-
 let counterValue = 0;
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
@@ -17,6 +12,3 @@ incrementBtn.addEventListener("click", () => {
   counterValue += 1;
   counter.innerHTML = counterValue;
 });
-
-// const counter = document.getElementById("value");
-// console.log(counter);
--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,3 @@
-// // Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input),
-// подставляет его текущее значение в span#name - output.Если инпут пустой,
-//     в спане должна отображаться строка "Anonymous".
-
 const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 const initValue = output.innerText;
@@ -12,10 +8,3 @@ input.addEventListener("input", (event) => {
     output.innerText = initValue;
   }
 });
-// input.addEventListener("keydown", (event) => {
-//   if (event.currentTarget.value.length) {
-//     output.innerText = event.currentTarget.value;
-//   } else {
-//     output.innerText = initValue;
-//   }
-// });
